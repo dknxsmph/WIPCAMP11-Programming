a7d9792f11addddb837e41a032814a6f282b92f8
dknxsmph <50834813+aofmini25@users.noreply.github.com>
2019-05-31 08:43:50 +0700

    Add Project

---
+++ WIP-XI/src/II/DataTypes/_3_String.java
@@ -0,0 +1,23 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package II.DataTypes;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _3_String {
+    public static void main(String[] args) {
+        String firstName = "Natthakan";
+        String lastName = "Thaweewatthanaprayun";
+        String nickName = "dknxsmph";
+        
+        System.out.println("firstName : " + firstName);
+        System.out.println("lastName : " + lastName);
+        System.out.println("nickName : " + nickName);
+        
+        System.out.println("Hi! my name is " + firstName + lastName + " (" + nickName + ").");
+    }
+}
